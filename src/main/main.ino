c3c9032d6f15d63f4606ac77fbfa9c2f92eb6ddf
Lukas Krämer <lukas.kraemer.2004@gmail.com>
2024-11-24 17:25:56 +0100

    Cooldowns working

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,11 +7,16 @@
 #define PIN_BUZZER 4
 
 #define THRESHOLD_PIEZO 30
-#define DELAY_BUTTON_DEBOUNCE_MS 20
+#define DELAY_BUTTON_DEBOUNCE_MS 50
 #define ALARM_TOGGLE_FREQ 20
 
-// after piezo detects movement, wait for DELAY_PIEZO_MOVED_MS and check if it is actually a button press
+// After piezo detects movement, wait for DELAY_PIEZO_MOVED_MS and check if it is actually a button press
 #define DELAY_PIEZO_MOVED_MS 500
+
+#define ATTENTION_COOLDOWN_MS 5000 // Cooldown to go back to sentry with no event in attention
+#define ALARM_COOLDOWN_MS 5000
+
+#define CLOCK_FREQ_MHZ 1
 
 enum State {
   DEEP_SLEEP,
@@ -20,7 +25,9 @@ enum State {
   ALARM
 };
 
-long long last_led_toggle = 0;
+long long timestamp_last_led_toggle;
+long long timestamp_entered_attention;
+long long timestamp_entered_alarm;
 bool led_state = true;
 
 State state = DEEP_SLEEP;
@@ -35,33 +42,44 @@ void setup() {
 void loop() {
   switch (state) {
     case DEEP_SLEEP:
+      digitalWrite(PIN_LED, LOW);
       sleep();
       state = SENTRY;
+      toggle(PIN_LED, 3, 100);
       break;
     case SENTRY:
-      analogWrite(PIN_LED, 10);
+      digitalWrite(PIN_LED, LOW);
       if (button_pressed()) {
-        digitalWrite(PIN_LED, LOW);
+        toggle(PIN_LED, 2, 400);
         state = DEEP_SLEEP;
       } else if (piezo_moved()) {
+        timestamp_entered_attention = _millis();
         state = ATTENTION;
       }
       break;
     case ATTENTION:
       digitalWrite(PIN_LED, HIGH);
+      if (_millis() - timestamp_entered_attention > ATTENTION_COOLDOWN_MS) {
+        state = SENTRY;
+      }
       if (button_pressed()) {
-        toggle(PIN_LED, 2, 300);
+        toggle(PIN_LED, 2, 400);
         state = DEEP_SLEEP;
       } else if (piezo_moved()) {
+        timestamp_entered_alarm = _millis();
         state = ALARM;
       }
       break;
     case ALARM:
+      if (_millis() - timestamp_entered_alarm > ALARM_COOLDOWN_MS) {
+        timestamp_entered_attention = _millis();
+        state = ATTENTION;
+      }
       if (button_pressed()) {
         toggle(PIN_LED, 2, 400);
         state = DEEP_SLEEP;
-      } else if (millis() - last_led_toggle > (1000 / ALARM_TOGGLE_FREQ)) {
-        last_led_toggle = millis();
+      } else if (_millis() - timestamp_last_led_toggle > (1000 / ALARM_TOGGLE_FREQ)) {
+        timestamp_last_led_toggle = _millis();
         led_state = !led_state;
         digitalWrite(PIN_LED, led_state);
       }
@@ -72,19 +90,19 @@ void loop() {
 bool button_pressed() {
   if (!digitalRead(PIN_BUTTON)) return false;
   while (digitalRead(PIN_BUTTON));
-  delay(DELAY_BUTTON_DEBOUNCE_MS);
+  _delay(DELAY_BUTTON_DEBOUNCE_MS);
   return true;
 }
 
 bool piezo_moved() {
   if (analogRead(PIN_PIEZO) <= THRESHOLD_PIEZO) return false;
 
-  // wait for potential button press
-  long long start_time = millis();
-  while (millis() - start_time < 200) {
+  // Wait for potential button press
+  long start_time = _millis();
+  while (_millis() - start_time < DELAY_PIEZO_MOVED_MS) {
     if (digitalRead(PIN_BUTTON)) {
       while (digitalRead(PIN_BUTTON));
-      delay(DELAY_BUTTON_DEBOUNCE_MS);
+      _delay(DELAY_BUTTON_DEBOUNCE_MS);
       return false;
     }
   }
@@ -94,21 +112,25 @@ bool piezo_moved() {
 void toggle(byte pin, int iterations, int t) {
   for (int i = 0; i < iterations; ++i) {
     digitalWrite(pin, HIGH);
-    delay(t);
+    _delay(t);
     digitalWrite(pin, LOW);
-    delay(t);
+    _delay(t);
   }
 }
 
 void sleep() {
   while (digitalRead(PIN_BUTTON));
-  delay(DELAY_BUTTON_DEBOUNCE_MS);
+  _delay(DELAY_BUTTON_DEBOUNCE_MS);
   GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
   PCMSK |= _BV(PCINT2);                   // Use PB2 as interrupt pin
   ADCSRA &= ~_BV(ADEN);                   // ADC off
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // Set sleep mode
 
-  PORTB &= ~_BV(PORTB2);                  // Ensure no pull-up on PB2 (external pull-down in use)
+  // Disable Brown-Out Detector
+  MCUCR |= (1 << BODS) | (1 << BODSE);
+  MCUCR &= ~(1 << BODSE);
+
+  ACSR |= _BV(ACD);                       // Disable analog comparator
 
   sleep_enable();                         // Enable sleep mode
   sei();                                  // Enable interrupts
@@ -119,11 +141,21 @@ void sleep() {
   sleep_disable();                        // Clear SE bit
   ADCSRA |= _BV(ADEN);                    // ADC on
 
+  ACSR &= ~_BV(ACD);                      // Re-enable Analog Comparator
   sei();                                  // Enable interrupts
   while (digitalRead(PIN_BUTTON));
-  delay(DELAY_BUTTON_DEBOUNCE_MS);
+  _delay(DELAY_BUTTON_DEBOUNCE_MS);
 }
 
 ISR(PCINT0_vect) {
-  // This is called when the interrupt occurs, but I don't need to do anything in it
+}
+
+void _delay(long long t) {
+  if (CLOCK_FREQ_MHZ == 1) delay(t * 8);
+  else if (CLOCK_FREQ_MHZ == 8) delay(t);
+}
+
+long long _millis() {
+  if (CLOCK_FREQ_MHZ == 1) return millis() / 8;
+  if (CLOCK_FREQ_MHZ == 8) return millis();
 }
